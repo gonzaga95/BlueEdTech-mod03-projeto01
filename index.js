b4351c4ab18ef0417648df91364e219e08272291
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-08 15:34:01 -0400

    req all e req by id

--- index.js
+++ index.js
@@ -11,24 +11,34 @@ const frota = [
         id: 1,
         tipo: 'Hatch',
         marca: 'Fiat',
-        veiculo: 'Uno',
+        modelo: 'Uno',
         placa: 'abc12d3',
     },
     {
         id: 2,
         tipo: 'Hatch',
         marca: 'Fiat',
-        veiculo: 'Uno',
+        modelo: 'Uno',
         placa: 'xyz12w3',
     },
     {
         id: 3,
         tipo: 'Picape',
         marca: 'Fiat',
-        veiculo: 'Strada',
+        modelo: 'Strada',
         placa: 'efg45h6',
     },
 ];
+
+app.get('/frota/todos-veiculos', (req, res) => {
+    res.send(frota);
+});
+
+app.get('/frota/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const veiculoID = frota.find((veiculo) => frota.id === id);
+    res.send(veiculoID);
+});
 
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
