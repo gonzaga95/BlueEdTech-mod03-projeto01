e234d08e6f572f4465f6e2598a37384a26efa2ee
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-08 19:58:21 -0400

    service com findAll e find by id

--- index.js
+++ index.js
@@ -9,40 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/frota', routes);
 
-const frota = [
-    {
-        id: 1,
-        tipo: 'Hatch',
-        marca: 'Fiat',
-        modelo: 'Uno',
-        placa: 'abc12d3',
-    },
-    {
-        id: 2,
-        tipo: 'Hatch',
-        marca: 'Fiat',
-        modelo: 'Uno',
-        placa: 'xyz12w3',
-    },
-    {
-        id: 3,
-        tipo: 'Picape',
-        marca: 'Fiat',
-        modelo: 'Strada',
-        placa: 'efg45h6',
-    },
-];
-
-app.get('/frota/veiculos', (req, res) => {
-    res.send(frota);
-});
-
-app.get('/frota/veiculo/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const veiculoID = frota.find((veiculo) => veiculo.id === id);
-    res.send(veiculoID);
-});
-
 app.app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
---
+++ src/services/frota.service.js
@@ -0,0 +1,31 @@
+const frota = [
+    {
+        id: 1,
+        tipo: 'Hatch',
+        marca: 'Fiat',
+        modelo: 'Uno',
+        placa: 'abc12d3',
+    },
+    {
+        id: 2,
+        tipo: 'Hatch',
+        marca: 'Fiat',
+        modelo: 'Uno',
+        placa: 'xyz12w3',
+    },
+    {
+        id: 3,
+        tipo: 'Picape',
+        marca: 'Fiat',
+        modelo: 'Strada',
+        placa: 'efg45h6',
+    },
+];
+
+const findAllVeiculosService = () => {
+    return frota;
+};
+
+const findVeiculoByIdService = (id) => {
+    return frota.find((veiculo) => veiculo.id == id);
+};
