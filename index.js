b6178d5eb4281a94a20a06a3d0e3fad2834238d0
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-08 15:28:29 -0400

    const frota

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+const frota = [
+    {
+        id: 1,
+        tipo: 'Hatch',
+        marca: 'Fiat',
+        veiculo: 'Uno',
+        placa: 'abc12d3',
+    },
+    {
+        id: 2,
+        tipo: 'Hatch',
+        marca: 'Fiat',
+        veiculo: 'Uno',
+        placa: 'xyz12w3',
+    },
+    {
+        id: 3,
+        tipo: 'Picape',
+        marca: 'Fiat',
+        veiculo: 'Strada',
+        placa: 'efg45h6',
+    },
+];
 
+app.listen(port, () => {
+    console.log(`O servidor está rodando em http://localhost:${port}`);
+});
